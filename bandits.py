Fix softmax and other adventures in Bandit Lane

bandits.py
@@ -15,14 +15,14 @@ REWARDS = 0
 TEMP = 30
 
 for t in range(TIMESTEP_MULTIPLIER * env.action_space.n):
-    softmax = np.exp(action_values/TEMP) / np.sum(np.exp(action_values)/TEMP)
+    softmax = np.exp(action_values/TEMP) / np.sum(np.exp(action_values/TEMP))
     action = choices(list(range(env.action_space.n)), softmax)[0]
     state, reward, _, _ = env.step(action)
     action_counts[action] += 1
     action_values[action] += (1 / action_counts[action]) * \
         (reward - action_values[action])
     REWARDS += reward
-    if TEMP > 0.001:
+    if TEMP > 0.1:
         TEMP *= 0.999
 
 MAX_REWARD = TIMESTEP_MULTIPLIER * env.action_space.n * \
@@ -32,6 +32,7 @@ print(f"\nTotal reward: {reward_percentage:.2f}%\n")
 data = {p: r[0] for p, r in zip(action_values, env.r_dist)}
 pprint(pd.DataFrame(data.items(), columns=["Predicted", "Real"]))
 print()
+softmax = [f"{prob:.10f}%" for prob in softmax]
 softmax = pd.DataFrame(softmax, columns=["Softmax"])
 softmax.index.name = "Arm"
 pprint(softmax)